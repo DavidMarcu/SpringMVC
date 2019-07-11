deb268c6dfc67c7eb9f6aae12461d57f3ad8c86d
DavidMarcu <david.marcu.1997@gmail.com>
2019-07-11 07:43:39 +0300

    Form submission with @RequestParam annotation

--- spring-mvc/src/main/java/com/springtutorial/springmvc/controller/HomeController.java
+++ spring-mvc/src/main/java/com/springtutorial/springmvc/controller/HomeController.java
@@ -3,6 +3,7 @@ package com.springtutorial.springmvc.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -18,6 +19,13 @@ public class HomeController {
     public String processForm(HttpServletRequest request, Model model){
         String name = request.getParameter("studentName");
         String result = "Hello " + name.toUpperCase() +"!";
+        model.addAttribute("greeting", result);
+        return "processed-form";
+    }
+
+    @RequestMapping("/processFormV2")
+    public String processFormV2(@RequestParam(value = "studentName")String name, Model model){
+        String result = "Hello from V2! " + name.toUpperCase() +"!";
         model.addAttribute("greeting", result);
         return "processed-form";
     }
